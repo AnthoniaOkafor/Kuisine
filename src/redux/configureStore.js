8de9a48339f1d1ed20748786ea39ff629e416063
AnthoniaOkafor <tonchuks@gmail.com>
2020-08-12 11:53:08 +0100

    Combining reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,3 +1,24 @@
+import {createStore, combineReducers} from 'redux';
+import { Dishes } from './dishes';
+import { Comments } from './comments';
+import { Promotions } from './promotions';
+import { Leaders } from './leaders';
+
+export const ConfigureStore = () => {
+    const store = createStore(
+        combineReducers({
+            dishes: Dishes,
+            comments: Comments,
+            promotions: Promotions,
+            leaders: Leaders
+        })
+    );
+
+    return store;
+}
+
+
+/*
 import {createStore} from 'redux';
 import { Reducer, initialState } from './reducer'
 
@@ -9,3 +30,4 @@ export const ConfigureStore = () => {
 
     return store;
 }
+*/
